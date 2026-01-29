52c976fe677903fec60a9cf861bd4cfc56b79f56
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2026-01-28 21:59:55 -0500

    Continuing to fix routing issue for subpages

--- my-site/src/App.tsx
+++ my-site/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio'
 import StarryBackground from './components/StarryBackground'
@@ -7,36 +7,19 @@ import Navbar from './components/Navbar';
 import Galaxy3DBackground from './components/backgrounds/Galaxy3DBackground';
 import ComingSoon from './components/ComingSoon';
 
-function AppContent() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const path = params.get("p");
-
-    if (path) {
-      navigate(path, { replace: true });
-    }
-  }, [navigate]);
-
-  return (
-    <div className='App'>
-      <StarryBackground />
-      <Galaxy3DBackground />
-      <Navbar/>
-      <Routes>
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/coming-soon" element={<ComingSoon />} />
-      </Routes>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
-      <AppContent />
+      <div className='App'>
+      <StarryBackground />
+      <Galaxy3DBackground />
+      <Navbar/>
+        <Routes>
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio/contact" element={<Contact />} />
+          <Route path="/portfolio/coming-soon" element={<ComingSoon />} />
+        </Routes>
+        </div>
     </Router> 
   );
 }
